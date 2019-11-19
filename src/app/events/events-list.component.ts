2c7f84760e2bc9374595658d48dc4f00a0704567
cindy <achiengcindy36@gmail.com>
2019-11-19 14:07:14 +0300

    using External templates to display events list page

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -4,19 +4,7 @@ import { Component } from '@angular/core';
 // inline html
 @Component({
     selector: 'events-list',
-    template: `
-    <div>
-    <h1>Upcoming  Angular Events</h1>
-    <hr>
-    <div class="well hoverwell thumbnail">    <h2>{{ event.name }}</h2>
-    <div>Date:  {{ event.date }}</div>
-    <div>Time:  {{ event.time }}</div>
-    <div>Price:  \${{ event.price }}</div>
-    <span> Location: {{ event.location.address }}</span>
-    <span> &nbsp </span>
-    <span> {{ event.location.city }} , {{ event.location.country }}</span>
-    </div>.
-    `
+    templateUrl: './events-list.component.html',
   })
   
   export class EventsListComponent {
